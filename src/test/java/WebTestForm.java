2a9de42334bf9aa98b78c78d76046ca50411bee3
Аленин Андрей <aleninmailbox@gmail.com>
2022-09-15 13:12:48 +0300

    Edit Data generate

--- src/test/java/WebTestForm.java
+++ src/test/java/WebTestForm.java
@@ -11,6 +11,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.net.SocketAddress;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
@@ -19,29 +21,36 @@ import static java.nio.channels.SocketChannel.open;
 public class WebTestForm {
 
     WebDriver driver;
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+    String planningDate = generateDate(4);
 
     @BeforeEach
     void setupTest(){
         Selenide.open("http://localhost:9999");
-    }
+        }
 
     @Test
     void testForm() {
         $("[placeholder='Город']").setValue("Санкт-Петербург");
+        String planningDate = generateDate(4);
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
-        $("[placeholder='Дата встречи']").setValue("17.09.2022");
+        $("[placeholder='Дата встречи']").setValue(planningDate);
         $("[name='name']").setValue("Аленин Андрей");
         $("[name='phone']").setValue("+79046472030");
         $("[role='presentation']").click();
         $(".button__content").click();
         $x("//*[contains(text(),'Успешно!')]").should(Condition.visible, Duration.ofSeconds(15));
-        $x("//*[contains(text(),'Встреча успешно забронирована на')]").should(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
     @Test
     void testFormWithoutCheckbox() {
         $("[placeholder='Город']").setValue("Санкт-Петербург");
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
-        $("[placeholder='Дата встречи']").setValue("17.09.2022");
+        $("[placeholder='Дата встречи']").setValue(planningDate);
         $("[name='name']").setValue("Аленин Андрей");
         $("[name='phone']").setValue("+79046472030");
         $(".button__content").click();
@@ -52,20 +61,22 @@ public class WebTestForm {
     void testFormName() {
         $("[placeholder='Город']").setValue("Санкт-Петербург");
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
-        $("[placeholder='Дата встречи']").setValue("17.09.2022");
+        $("[placeholder='Дата встречи']").setValue(planningDate);
         $("[name='name']").setValue("Аленин-Огарков Андрей");
         $("[name='phone']").setValue("+79046472030");
         $("[role='presentation']").click();
         $(".button__content").click();
         $x("//*[contains(text(),'Успешно!')]").should(Condition.visible, Duration.ofSeconds(15));
-        $x("//*[contains(text(),'Встреча успешно забронирована на')]").should(Condition.visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
     @Test
     void testFormWrongCity() {
         $("[placeholder='Город']").setValue("НьюЙорк");
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
-        $("[placeholder='Дата встречи']").setValue("17.09.2022");
+        $("[placeholder='Дата встречи']").setValue(planningDate);
         $("[name='name']").setValue("Аленин Андрей");
         $("[name='phone']").setValue("+79046472030");
         $("[role='presentation']").click();
@@ -75,9 +86,10 @@ public class WebTestForm {
 
     @Test
     void testFormWrongDate() {
+        String planningDate = generateDate(1);
         $("[placeholder='Город']").setValue("Санкт-Петербург");
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
-        $("[placeholder='Дата встречи']").setValue("10.09.2022");
+        $("[placeholder='Дата встречи']").setValue(planningDate);
         $("[name='name']").setValue("Аленин Андрей");
         $("[name='phone']").setValue("+79046472030");
         $("[role='presentation']").click();
@@ -89,7 +101,7 @@ public class WebTestForm {
     void testFormWrongName() {
         $("[placeholder='Город']").setValue("Санкт-Петербург");
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
-        $("[placeholder='Дата встречи']").setValue("17.09.2022");
+        $("[placeholder='Дата встречи']").setValue(planningDate);
         $("[name='name']").setValue("Alenin Andrew");
         $("[name='phone']").setValue("+79046472030");
         $("[role='presentation']").click();
@@ -101,7 +113,7 @@ public class WebTestForm {
     void testFormWrongPhoneNumber() {
         $("[placeholder='Город']").setValue("Санкт-Петербург");
         $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.DELETE);
-        $("[placeholder='Дата встречи']").setValue("17.09.2022");
+        $("[placeholder='Дата встречи']").setValue(planningDate);
         $("[name='name']").setValue("Аленин Андрей");
         $("[name='phone']").setValue("89046472030");
         $("[role='presentation']").click();
@@ -109,7 +121,6 @@ public class WebTestForm {
         $x("//*[contains(text(),'Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.')]");
     }
 
-    //пустая форма без чек бокса
     @Test
     void testFormEmptyWithCheckbox() {
         $("[placeholder='Город']").setValue("");
